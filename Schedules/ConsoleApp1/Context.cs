16c62a2a753212356c9d48981b4dc37888f73cf2
xd43vild69 <xd43vild69@gmail.com>
2019-04-06 18:35:49 -0500

    Init project test 6:35pm Sat

---
+++ Schedules/ConsoleApp1/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Schedules
+{
+    public class Context : DbContext
+    {
+        public Context() : base("StringConn")
+        {
+
+        }
+        public DbSet<Person > Patients { get; set; }
+    }
+}
---
+++ Schedules/ConsoleApp1/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace ConsoleApp1.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ConsoleApp1.Context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(ConsoleApp1.Context context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
