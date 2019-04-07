3209d0e5fe4c72773498afa4a6e2d91d4062bd0b
xd43vild69 <xd43vild69@gmail.com>
2019-04-06 19:21:35 -0500

    Se agrega la libreria de clases que contiene las reglas de negocio del lado del servidor

--- Schedules/BAL/ScheduleBAL.cs
+++ Schedules/BAL/ScheduleBAL.cs
@@ -3,10 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using DTO;
+using SAL.Context;
 namespace BAL
 {
-    class ScheduleBAL
+    public class ScheduleBAL
     {
+        private ScheduleRepository<Schedule> Repository { get; set;}
+
+        public ScheduleBAL()
+        {
+            Repository = new ScheduleRepository<Schedule>();
+        }
+
+        public Schedule GetDates(int idPatient)
+        {
+            return Repository.GetById(idPatient);
+        }
+
+        public IEnumerable<Schedule> GetByIdPatient(int idPatient)
+        {
+            return Repository.GetByIdPatient(idPatient);
+        }
+
+
+        public void Delete(int idPatient)
+        {
+            //TODO: validation needed before Delete it.
+            Repository.Delete(idPatient);
+        }
+
+        public void Insert(Schedule schedule)
+        {
+            //TODO: validation needed before Insert it.
+            Repository.Insert(schedule);
+        }
+
+        /// <summary>
+        /// Valid that the date is greater than 24 hours.
+        /// Basic example to complex business logic here.
+        /// Need UnitTest call.
+        /// </summary>
+        /// <param name="idPatient">Identifier</param>
+        /// <returns>True if the process could continue.</returns>
+        public bool IsDateValidToCancel(int idPatient)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Patient can be schedule a date if exist another date the same day.
+        /// Basic example to complex business logic here.
+        /// Need UnitTest call.
+        /// </summary>
+        /// <param name="idPatient">Identifier</param>
+        /// <param name="datebook">Time to schedule</param>
+        /// <returns>True if the process could continue.</returns>
+        public bool IsPatientWithDatesSameDay(int idPatient, DateTime datebook)
+        {
+            return true;
+        }
     }
 }
---
+++ Schedules/BAL/TypeScheduleBAL.cs
@@ -0,0 +1,20 @@
+﻿using DTO;
+using SAL.Context;
+using System.Collections.Generic;
+
+namespace BAL
+{
+    public class TypeScheduleBAL
+    {
+        public TypeScheduleRepository<TypeSchedule> Repository { get; set; }
+
+        public TypeScheduleBAL()
+        {
+            Repository = new TypeScheduleRepository<TypeSchedule>();
+        }
+        public IEnumerable<TypeSchedule> GetLists()
+        {
+            return Repository.GetLists();
+        }
+    }
+}
